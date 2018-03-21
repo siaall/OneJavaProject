b452715cf31b2c6c9530e22b00bb7f380490f538
siaall <faczchen@gmail.com>
2018-03-21 09:03:25 +0800

    1111

---
+++ src/main/java/appJarTest/Test001.java
@@ -0,0 +1,29 @@
+package appJarTest;
+
+import appJar.TestJar;
+
+public class Test001 {
+
+	/**
+	 * @Title: main
+	 * @author: Siaall
+	 * @param args
+	 * @date: 2018-03-15 16:23
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+
+     TestJar tt = new TestJar();
+     String name ="Emma";
+      tt.fn1(name);
+		
+		
+		
+		
+
+	}
+	/**
+	 * @author: Siaall
+	 * @date: 2018-03-15 16:23
+	 */
+}
